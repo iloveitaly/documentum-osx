4401e8959379a3ebbf2e45bd6ba9219e387dbf4e
Michael Bianco <iloveitaly@gmail.com>
2011-10-14 11:54:37 -0400

    Indexer seems tobe working

--- Source/Bundles/Ruby.docbundle/index.rb
+++ Source/Bundles/Ruby.docbundle/index.rb
@@ -6,6 +6,27 @@ class DocIndexHelper
   require 'rubygems'
   require 'FileUtils'
   require 'nokogiri'
+  require 'json'
+  
+  @docs_dir = "docs"
+  @strip_javascript = true
+  
+  def initialize
+    @docs_dir = "docs"
+    @docs_path = File.join($plugin_directory, @docs_dir)
+    @process_element_name = :process_element_name
+  end
+  
+  def process_element_name(name, level)
+    name = name.strip
+    rootName = name[/^[a-zA-Z_:]+/]
+    
+    if rootName.nil?
+      puts "No match for #{name}"
+    end
+    
+    rootName
+  end
   
   def rename_uncompressed_docs
     docs = Dir['ruby_?_?_?_core']
@@ -17,7 +38,7 @@ class DocIndexHelper
   def fix_asset_references
     asset_converter = Hash.new
     
-    Dir.chdir("docs")
+    Dir.chdir(@docs_dir)
     file_list = Dir.glob("**/*.*").reject {|fn| File.directory?(fn) }
     
     # generate the converter list
@@ -30,14 +51,17 @@ class DocIndexHelper
     end
     
     file_list.each do |f|
-      absoluteFilePath = File.join($plugin_directory, "docs", f)
+      absoluteFilePath = File.join($plugin_directory, @docs_dir, f)
       
       if File.extname(f) == ".html"
           doc = Nokogiri::HTML(File.open(absoluteFilePath))
-          doc.xpath("//script").remove
+          
+          # http://stackoverflow.com/questions/1980845/removing-the-script-elements-of-an-html
+          @strip_javascript ? doc.xpath("//script").remove : nil
+          
           doc.xpath("//link").each do |link|
             # convert the link paths
-            link["href"] = asset_converter[File.basename(link["href"])]
+            link["href"] = ("../" * (f.count "/")) + asset_converter[File.basename(link["href"])]
           end
           
           # write the converted file
@@ -45,14 +69,58 @@ class DocIndexHelper
       end
     end
   end
-
-  def strip_javascript
-    # http://stackoverflow.com/questions/1980845/removing-the-script-elements-of-an-html
-  end  
+  
+  def generate_structure(*heiarchy)
+    Dir.chdir(@docs_path)
+    
+    heiarchicalElements = Hash.new
+    currentHeiarchyReference = nil
+    lastHeiarchyKey = nil
+    
+    fileList = Dir.glob("**/*.html").reject {|fn| File.directory?(fn) }.each do |helpFile|
+      absoluteFilePath = File.join(@docs_path, helpFile)
+      helpDoc = Nokogiri::HTML(File.open(absoluteFilePath))
+      
+      # reset heirachical variables
+      currentHeiarchyReference = heiarchicalElements
+      lastHeiarchyKey = nil
+      
+      # move through the heiarchy for this page
+      heiarchy.each_index do |index|
+        # find current heiarchy position & set empty hashs
+        # note that this only works for 2 levels deep currently
+        # to work with more levels there would have to be a 'find parent match' method
+        currentHeiarchyReference = heiarchicalElements
+        heiarchyDepth = index
+        while heiarchyDepth > 0
+            break unless not lastHeiarchyKey.nil?
+            currentHeiarchyReference = currentHeiarchyReference[lastHeiarchyKey]["children"] = Hash.new
+            heiarchyDepth -= 1
+        end
+        
+        # find all the elements corresponding to this depth level
+        helpDoc.css(heiarchy[index]).each do |helpElement|
+          helpElementName = self.send(@process_element_name, helpElement.content, index)
+          
+          # skip empty entries
+          next unless not (helpElementName.nil? || helpElementName.empty?)
+          
+          currentHeiarchyReference[helpElementName] = {:path => absoluteFilePath, :title => helpElementName}
+          
+          # find associated anchor
+          
+          lastHeiarchyKey = helpElementName
+        end
+        
+        # search for matching elements
+      end      
+    end
+    
+    puts JSON.pretty_generate(heiarchicalElements)
+  end
 end
 
-puts "hello"
-
 ih = DocIndexHelper.new
-ih.rename_uncompressed_docs
-ih.fix_asset_references
+# ih.rename_uncompressed_docs
+# ih.fix_asset_references
+ih.generate_structure "h1.class, h1.module", "div.method-heading span.method-callseq"
