a5a3a73ce3670c75dc68bc86ebed28089aeb999e
Michael Bianco <iloveitaly@gmail.com>
2011-11-05 14:16:11 -0400

    Adding man pages bundle

--- Source/Bundles/ManPages.docbundle/index.rb
+++ Source/Bundles/ManPages.docbundle/index.rb
@@ -2,28 +2,6 @@
 
 require File.dirname(__FILE__) + '/../documentum'
 
-# https://github.com/yimmu/Man-to-html/blob/master/man-2-html.py
-# 
-# def man_to_html(manpage)  
-#   if manpage[-2:] == "gz"
-#     data = os.popen("gunzip -cd " + manpage + "| groff -t -man -Thtml 2>/dev/null", "r")
-#   else:
-#     data = os.popen("groff -t -man -Thtml " + manpage + " 2>/dev/null", "r")
-#     
-#   b = manpage.split("/")[-2]
-#   c = b+"/"+manpage.split("/")[-1]
-#   
-#   if not os.access(outdir, os.F_OK):
-#     os.mkdir(outdir)
-#     
-#   if not os.access(outdir+"/"+b, os.F_OK):
-#     os.mkdir(outdir+"/"+b)
-#     
-#   f = open(outdir+"/"+c+".html", "w")
-#   for lines in data:
-#     f.write(lines)    
-#   f.close
-
 ih = DocumentationIndexHelper.new
 
 man_paths = ["/usr/share/man", "/usr/local/man", "/usr/share/man", "/opt/local/share/man"]
@@ -42,18 +20,10 @@ man_paths.reject{|man_dir| not File.exists? man_dir }.each do |man_dir|
       man_html = %x[groff -t -man -Thtml "#{man_file}" 2>/dev/null]
     end
     
+    puts "Saving #{man_save_name}"
+    
     File.open(File.join(ih.docs_path, man_save_name), "w") {|file| file.puts man_html}
     
     man_file_list << man_file
   end
 end
-# 
-# man_paths = glob.glob(path + "man[0-9,a-z,A-Z]/*")
-# man_paths.sort()
-# 
-# for i in range(len(man_paths)):
-#   if os.access(man_paths[i], os.F_OK):
-#     manpage = os.listdir(man_paths[i])
-#     manpage.sort()
-#     for y in range(len(manpage)):
-#      man_to_html(man_paths[i]+"/"+manpage[y])
